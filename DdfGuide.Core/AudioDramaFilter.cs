1532fa872f0793d700b805d6918fcf9529c3e379
Marcel Neumann <mail@marcelneumann.eu>
2018-10-07 13:39:29 +0200

    Added AudioDramaFilter with tests.

---
+++ DdfGuide.Core/AudioDramaFilter.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace DdfGuide.Core
+{
+    public class AudioDramaFilter
+    {
+        public bool FilterMainAudioDramas { get; set; }
+
+        private readonly IEnumerable<AudioDrama> _audioDramas;
+
+        public AudioDramaFilter(IEnumerable<AudioDrama> audioDramas)
+        {
+            _audioDramas = audioDramas;
+        }
+        
+        public IEnumerable<AudioDrama> Filter()
+        {
+            var filtered = _audioDramas;
+
+            if (FilterMainAudioDramas)
+            {
+                filtered = filtered.Where(x => x.AudioDramaDto.Number.HasValue == false);
+            }
+
+            return filtered;
+        }
+    }
+}
---
+++ DdfGuide.Test/AudioDramaFilterTests.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using DdfGuide.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DdfGuide.Test
+{
+    [TestClass]
+    public class AudioDramaFilterTests
+    {
+        private AudioDramaFilter _sut;
+        private List<AudioDrama> _audioDramas;
+
+        [TestInitialize]
+        public void Init()
+        {
+            var provider = new MultipleAudioDramaProvider();
+            _audioDramas = provider.Get().ToList();
+            _sut = new AudioDramaFilter(_audioDramas);
+        }
+
+        [TestMethod]
+        public void Default_FilterNoItems()
+        {
+            var filtered = _sut.Filter().ToList();
+
+            CollectionAssert.AreEqual(_audioDramas, filtered);
+        }
+
+        [TestMethod]
+        public void FilterMainAudioDramas_true()
+        {
+            _sut.FilterMainAudioDramas = true;
+
+            var filtered = _sut.Filter().ToList();
+            var special = _audioDramas.Where(x => !x.AudioDramaDto.Number.HasValue).ToList();
+
+            CollectionAssert.AreEqual(special, filtered);
+        }
+
+        [TestMethod]
+        public void FilterMainAudioDramas_false()
+        {
+            _sut.FilterMainAudioDramas = false;
+
+            var filtered = _sut.Filter().ToList();
+
+            CollectionAssert.AreEqual(_audioDramas, filtered);
+        }
+    }
+}
