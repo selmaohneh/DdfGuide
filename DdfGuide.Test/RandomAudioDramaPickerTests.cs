13c263ecee28053eef6539abedc73cca7d2431aa
Marcel Neumann <mail@marcelneumann.eu>
2018-10-11 12:49:56 +0200

    Add test for random audio drama picker

---
+++ DdfGuide.Test/RandomAudioDramaPickerTests.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using DdfGuide.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DdfGuide.Test
+{
+    [TestClass]
+    public class RandomAudioDramaPickerTests
+    {
+        [TestMethod]
+        public void RandomlyPickedAudioDramaIsOneOfTheGivenAudioDramas()
+        {
+            var provider = new SampleAudioDramaProvider();
+            var audioDramas = provider.Get().ToList();
+            var picker = new RandomAudioDramaPicker();
+
+           var random = picker.Pick(audioDramas);
+
+            Assert.IsTrue(audioDramas.Contains(random));
+        }
+    }
+}
