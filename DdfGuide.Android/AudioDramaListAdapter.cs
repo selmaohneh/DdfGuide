f8647812dd250889c7d0f4263784f913dc095d4c
Marcel Neumann <mail@marcelneumann.eu>
2018-10-14 19:22:44 +0200

    Use dto ToString instead of plain title in list view

--- DdfGuide.Android/AudioDramaListAdapter.cs
+++ DdfGuide.Android/AudioDramaListAdapter.cs
@@ -33,7 +33,7 @@ namespace DdfGuide.Android
                 view = _context.LayoutInflater.Inflate(Resource.Layout.audiodramalistitem, null);
             }
             
-            view.FindViewById<TextView>(Resource.Id.titleview).Text = audioDrama.AudioDramaDto.Title;
+            view.FindViewById<TextView>(Resource.Id.titleview).Text = audioDrama.AudioDramaDto.ToString();
             //view.FindViewById<ImageView>(Resource.Id.Image).SetImageResource(item.ImageResourceId);
             return view;
         }
