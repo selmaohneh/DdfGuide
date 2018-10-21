e85147585da8996d90e67299ee6a5a13751d2985
Marcel Neumann <mail@marcelneumann.eu>
2018-10-21 17:37:16 +0200

    Started with single view. Add cover, checkboxes and description

--- DdfGuide.Android/AudioDramaView.cs
+++ DdfGuide.Android/AudioDramaView.cs
@@ -4,6 +4,8 @@ using Android.OS;
 using Android.Views;
 using Android.Widget;
 using DdfGuide.Core;
+using FFImageLoading;
+using FFImageLoading.Views;
 
 namespace DdfGuide.Android
 {
@@ -14,14 +16,27 @@ namespace DdfGuide.Android
         public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
         {
             _view = inflater.Inflate(Resource.Layout.audiodramalayout, container, false);
+
+            var toolbar = _view.FindViewById<Toolbar>(Resource.Id.toolbarsingleview);
+            toolbar.SetNavigationIcon(Resource.Mipmap.arrow_back);
+            toolbar.NavigationOnClick += (sender, args) => BackClicked?.Invoke(this, EventArgs.Empty);
+
             return _view;
         }
 
         public event EventHandler BackClicked;
         public void SetAudioDrama(AudioDrama audioDrama)
         {
-            var titleView = _view.FindViewById<TextView>(Resource.Id.textviewtitel);
+            var titleView = _view.FindViewById<TextView>(Resource.Id.textviewtitelsingle);
             titleView.Text = audioDrama.AudioDramaDto.Title;
+
+            var descriptionView = _view.FindViewById<TextView>(Resource.Id.textViewDescription);
+            descriptionView.Text = audioDrama.AudioDramaDto.Description;
+
+            var imageView = _view.FindViewById<ImageViewAsync>(Resource.Id.coverviewsingle);
+            ImageService.Instance
+                .LoadUrl(audioDrama.AudioDramaDto.CoverUrl)
+                .IntoAsync(imageView);
         }
 
         public event EventHandler HeardChanged;
