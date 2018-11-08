5a6b296f9e1cd014e46d66b2df16249cc5145c2f
Marcel Neumann <mail@marcelneumann.eu>
2018-11-08 18:05:38 +0100

    Added empty default parser

---
+++ DdfGuide.Parser/RockyBeachDieDreiFragezeichenParser.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using DdfGuide.Core;
+
+namespace DdfGuide.Parser
+{
+    public class RockyBeachDieDreiFragezeichenParser : IAudioDramaDtoParser
+    {
+        public bool TryParseNumber(out int number)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool TryParseTitle(out string title)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool TryParseReleaseDate(out DateTime releaseDate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool TryParseCoverUrl(out string coverUrl)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool TryParseDescription(out string description)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool TryParseRoles(out IEnumerable<RoleDto> roles)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool TryParseInterpreter(out string interpreter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
