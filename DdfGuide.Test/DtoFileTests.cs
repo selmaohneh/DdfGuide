aefb50453b538c6b401e3f9cc85282a7a579b228
Marcel Neumann <mail@marcelneumann.eu>
2018-11-11 08:47:22 +0100

    Added test to check for duplicate IDs in dto file.

---
+++ DdfGuide.Test/DtoFileTests.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using DdfGuide.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+
+namespace DdfGuide.Test
+{
+    [TestClass]
+    public class DtoFileTests
+    {
+        [TestMethod]
+        public void DtoFileContainsNoDuplicateIds()
+        {
+            var json = File.ReadAllText("../../../dtos.json");
+            var dtos = JsonConvert.DeserializeObject<IEnumerable<AudioDramaDto>>(json);
+
+            var duplicates = dtos.GroupBy(x => x.Id).Where(x => x.Count() > 1);
+
+            Assert.IsFalse(duplicates.Any());
+        }
+    }
+}
