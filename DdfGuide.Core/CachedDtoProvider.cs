fb8e91751708a8138cda8e7d3b2ca8561a31f2c7
Marcel Neumann <mail@marcelneumann.eu>
2018-11-20 17:20:58 +0100

    Added cache and cached provider.

---
+++ DdfGuide.Core/CachedDtoProvider.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DdfGuide.Core
+{
+    public class CachedDtoProvider : IProvider<IEnumerable<AudioDramaDto>>
+    {
+        private readonly ICache<IEnumerable<AudioDramaDto>> _cache;
+        private readonly IProvider<IEnumerable<AudioDramaDto>> _liveProvider;
+
+        public CachedDtoProvider(
+            ICache<IEnumerable<AudioDramaDto>> cache, 
+            IProvider<IEnumerable<AudioDramaDto>> liveProvider)
+        {
+            _cache = cache;
+            _liveProvider = liveProvider;
+        }
+
+        public IEnumerable<AudioDramaDto> Get()
+        {
+            var cachedItems = _cache.Load().ToList();
+            if (cachedItems.Any())
+            {
+                return cachedItems;
+            }
+
+            var liveItems = _liveProvider.Get().ToList();
+
+            _cache.Save(liveItems);
+
+            return liveItems;
+        }
+    }
+}
---
+++ DdfGuide.Core/ICache.cs
@@ -0,0 +1,8 @@
+﻿namespace DdfGuide.Core
+{
+    public interface ICache<T>
+    {
+        T Load();
+        void Save(T items);
+    }
+}
