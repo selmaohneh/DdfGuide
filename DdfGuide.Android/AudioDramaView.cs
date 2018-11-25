0895cb94badfa99f94081b025d8cfeb8d7f515d7
Marcel Neumann <mail@marcelneumann.eu>
2018-11-25 10:37:52 +0100

    Fixed crash when opening audio drama with no description.

--- DdfGuide.Android/AudioDramaView.cs
+++ DdfGuide.Android/AudioDramaView.cs
@@ -57,7 +57,10 @@ namespace DdfGuide.Android
 
             var descriptionView = _view.FindViewById<TextView>(Resource.Id.textViewDescription);
             descriptionView.Text = string.Empty;
-            descriptionView.Append(Html.FromHtml(audioDrama.AudioDramaDto.Description, FromHtmlOptions.ModeLegacy));
+            if (audioDrama.AudioDramaDto.Description != null)
+            {
+                descriptionView.Append(Html.FromHtml(audioDrama.AudioDramaDto.Description, FromHtmlOptions.ModeLegacy));
+            }
 
             var releaseDateView = _view.FindViewById<TextView>(Resource.Id.textviewreleasesingle);
             releaseDateView.Text = audioDrama.AudioDramaDto.ReleaseDate.ToShortDateString();
