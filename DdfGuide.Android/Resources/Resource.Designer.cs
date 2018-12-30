72c31e5bacd6f64b528b53099eff19de245fca9a
Marcel Neumann <mail@marcelneumann.eu>
2018-12-30 09:27:55 +0100

    Added permissions to manisfest

--- DdfGuide.Android/Resources/Resource.Designer.cs
+++ DdfGuide.Android/Resources/Resource.Designer.cs
@@ -3046,34 +3046,58 @@ namespace DdfGuide.Android
 			public const int arrow_back = 2130903040;
 			
 			// aapt resource value: 0x7f030001
-			public const int baseline_check_box_black_24 = 2130903041;
+			public const int baseline_check_box_24 = 2130903041;
 			
 			// aapt resource value: 0x7f030002
-			public const int baseline_check_box_outline_blank_black_24 = 2130903042;
+			public const int baseline_check_box_black_24 = 2130903042;
 			
 			// aapt resource value: 0x7f030003
-			public const int baseline_favorite_black_24 = 2130903043;
+			public const int baseline_check_box_outline_blank_24 = 2130903043;
 			
 			// aapt resource value: 0x7f030004
-			public const int baseline_favorite_border_black_24 = 2130903044;
+			public const int baseline_check_box_outline_blank_black_24 = 2130903044;
 			
 			// aapt resource value: 0x7f030005
-			public const int baseline_filter_list_white_24 = 2130903045;
+			public const int baseline_favorite_24 = 2130903045;
 			
 			// aapt resource value: 0x7f030006
-			public const int baseline_search_white_24 = 2130903046;
+			public const int baseline_favorite_black_24 = 2130903046;
 			
 			// aapt resource value: 0x7f030007
-			public const int baseline_sort_white_24 = 2130903047;
+			public const int baseline_favorite_border_24 = 2130903047;
 			
 			// aapt resource value: 0x7f030008
-			public const int ic_dice_3_white_24dp = 2130903048;
+			public const int baseline_favorite_border_black_24 = 2130903048;
 			
 			// aapt resource value: 0x7f030009
-			public const int ic_launcher = 2130903049;
+			public const int baseline_filter_list_24 = 2130903049;
 			
 			// aapt resource value: 0x7f03000a
-			public const int ic_launcher_round = 2130903050;
+			public const int baseline_filter_list_white_24 = 2130903050;
+			
+			// aapt resource value: 0x7f03000b
+			public const int baseline_search_24 = 2130903051;
+			
+			// aapt resource value: 0x7f03000c
+			public const int baseline_search_white_24 = 2130903052;
+			
+			// aapt resource value: 0x7f03000d
+			public const int baseline_sort_24 = 2130903053;
+			
+			// aapt resource value: 0x7f03000e
+			public const int baseline_sort_white_24 = 2130903054;
+			
+			// aapt resource value: 0x7f03000f
+			public const int dice_3 = 2130903055;
+			
+			// aapt resource value: 0x7f030010
+			public const int ic_dice_3_white_24dp = 2130903056;
+			
+			// aapt resource value: 0x7f030011
+			public const int ic_launcher = 2130903057;
+			
+			// aapt resource value: 0x7f030012
+			public const int ic_launcher_round = 2130903058;
 			
 			static Mipmap()
 			{
