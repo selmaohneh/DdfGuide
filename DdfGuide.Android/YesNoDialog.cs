c36ed3eed20819a1cb5790599cf97607cf92d2a6
Marcel Neumann <mail@marcelneumann.eu>
2019-04-24 15:11:03 +0200

    Implemented a yes/no dialog to ask the user whether he really wants to export the user data to the clipboard. #12

---
+++ DdfGuide.Android/YesNoDialog.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using DdfGuide.Core;
+
+namespace DdfGuide.Android
+{
+    public class YesNoDialog : IYesNoDialog
+    {
+        private readonly Context _context;
+
+        public YesNoDialog(Context context)
+        {
+            _context = context;
+        }
+
+        public event EventHandler YesClicked;
+        public event EventHandler NoClicked;
+
+        public void Show(string title, string message)
+        {
+            var builder = new AlertDialog.Builder(_context);
+
+            builder
+                .SetMessage(message)
+                .SetTitle(title)
+                .SetPositiveButton("Ja", (sender, args) => YesClicked?.Invoke(this, EventArgs.Empty))
+                .SetNegativeButton("Nein", (sender, args) => NoClicked?.Invoke(this, EventArgs.Empty))
+                .Show();
+        }
+    }
+}
--- DdfGuide.Core/UserDataImportExport.cs
+++ DdfGuide.Core/UserDataImportExport.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using Newtonsoft.Json;
 
 namespace DdfGuide.Core
@@ -7,21 +8,29 @@ namespace DdfGuide.Core
     {
         private readonly ICache<IEnumerable<AudioDramaUserData>> _userDataCache;
         private readonly IClipboardService _clipboardService;
+        private readonly IYesNoDialog _yesNoDialog;
 
         public UserDataImportExport(
             ICache<IEnumerable<AudioDramaUserData>> userDataCache,
-            IClipboardService clipboardService)
+            IClipboardService clipboardService,
+            IYesNoDialog yesNoDialog)
         {
             _userDataCache = userDataCache;
             _clipboardService = clipboardService;
+            _yesNoDialog = yesNoDialog;
         }
 
         public void ExportUserData()
         {
-            var userData = _userDataCache.Load();
+            var userData = _userDataCache.Load().ToList();
             var json = JsonConvert.SerializeObject(userData);
 
-            _clipboardService.SetClipboardText(json);
+            _yesNoDialog.YesClicked += (sender, args) => { _clipboardService.SetClipboardText(json); };
+
+            _yesNoDialog.Show(
+                "In Zwischenablage exportieren?",
+                $"Gehört: {userData.Count(x => x.Heard)}\n" +
+                $"Favoriten: {userData.Count(x => x.IsFavorite)}");
         }
     }
 }
