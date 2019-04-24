74da7386089a46286226ec1cf83dba29cc2f0aef
Marcel Neumann <mail@marcelneumann.eu>
2019-04-24 13:27:27 +0200

    Added class for handling import and export of user data. Implemented first version of exporting to clipboard. Test included. #12

---
+++ DdfGuide.Core/UserDataImportExport.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace DdfGuide.Core
+{
+    public class UserDataImportExport
+    {
+        private readonly ICache<IEnumerable<AudioDramaUserData>> _userDataCache;
+        private readonly IClipboardService _clipboardService;
+
+        public UserDataImportExport(
+            ICache<IEnumerable<AudioDramaUserData>> userDataCache,
+            IClipboardService clipboardService)
+        {
+            _userDataCache = userDataCache;
+            _clipboardService = clipboardService;
+        }
+
+        public void ExportUserData()
+        {
+            var userData = _userDataCache.Load();
+            var json = JsonConvert.SerializeObject(userData);
+
+            _clipboardService.SetClipboardText(json);
+        }
+    }
+}
---
+++ DdfGuide.Test/UserDataImportExportTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using DdfGuide.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Moq.AutoMock;
+using Newtonsoft.Json;
+
+namespace DdfGuide.Test
+{
+    [TestClass]
+    public class UserDataImportExportTests
+    {
+        [TestMethod]
+        public void ExportUserData()
+        {
+            var mocker = new AutoMocker();
+
+            var userData = new List<AudioDramaUserData>
+            {
+                new AudioDramaUserData(Guid.Empty, true, false),
+                new AudioDramaUserData(Guid.Empty, false, true),
+            };
+
+            mocker
+                .GetMock<ICache<IEnumerable<AudioDramaUserData>>>()
+                .Setup(x => x.Load())
+                .Returns(userData);
+
+            var sut = mocker.CreateInstance<UserDataImportExport>();
+
+            sut.ExportUserData();
+
+            mocker.Verify<ICache<IEnumerable<AudioDramaUserData>>>(x => x.Load(), Times.Once);
+
+            var json = JsonConvert.SerializeObject(userData);
+            mocker.Verify<IClipboardService>(x => x.SetClipboardText(json), Times.Once);
+        }
+    }
+}
