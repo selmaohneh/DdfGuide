97fecfdf4b416908e93de0244f4277ecbab52525
Marcel Neumann <mail@marcelneumann.eu>
2020-09-30 09:47:23 +0200

    Added test for constructing the spotify uir.

---
+++ DdfGuide.Test/AudioDramaDtoTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using DdfGuide.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DdfGuide.Test
+{
+    [TestClass]
+    public class AudioDramaDtoTests
+    {
+        [TestMethod]
+        public void GivenSpotifyId_CorrectSpotifyUri()
+        {
+            var sut = new AudioDramaDto(
+                Guid.Empty,
+                string.Empty,
+                -1,
+                DateTime.Now,
+                string.Empty,
+                string.Empty,
+                string.Empty,
+                Enumerable.Empty<RoleDto>(),
+                "HomerSimpson");
+
+            var uri = sut.SpotifyUri;
+
+            Assert.AreEqual(@"https://open.spotify.com/album/HomerSimpson", uri.ToString());
+        }
+    }
+}
