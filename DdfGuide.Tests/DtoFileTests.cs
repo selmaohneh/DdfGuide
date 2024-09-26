2b89279d02855f118df3b06d688d9186712e810b
El Cello <marcel.neumann@draeger.com>
2024-09-26 09:30:54 +0200

    add tests for special characters, epsiode amount and empty roles

--- DdfGuide.Tests/DtoFileTests.cs
+++ DdfGuide.Tests/DtoFileTests.cs
@@ -23,5 +23,30 @@ namespace DdfGuide.Tests
 
             Assert.IsFalse(duplicates.Any());
         }
+
+        [TestMethod]
+        public void DtoFileContainsAtLeast300Episodes()
+        {
+            var enoughEpisodesExist = _dtos.ToList().Count > 300;
+
+            Assert.IsTrue(enoughEpisodesExist);
+        }
+
+        [TestMethod]
+        public void DtoFileContainsTitlesWithSpecialCharacters()
+        {
+            var episode229 = _dtos.Single(x => x.NumberEuropa == 229 && x.Interpreter == "Die drei ???");
+            Assert.AreEqual("Drehbuch der Täuschung", episode229.Title);
+
+            var episode205 = _dtos.Single(x => x.NumberEuropa == 205 && x.Interpreter == "Die drei ???");
+            Assert.AreEqual("Das rätselhafte Erbe", episode205.Title);
+        }
+
+        [TestMethod]
+        public void DtoFileContainsNoEmptyRoles()
+        {
+            var containsEmptyRoles = _dtos.Any(x => x.Roles.Any(y => string.IsNullOrWhiteSpace(y.Speaker) || string.IsNullOrWhiteSpace(y.Character)));
+            Assert.IsFalse(containsEmptyRoles);
+        }
     }
 }
